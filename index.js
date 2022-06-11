e6d2227237f52433b5abe061716911d63641f407
Syed Anas <59249245+Syedanas-ctrl@users.noreply.github.com>
2022-06-12 03:47:06 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+
+const app = express();
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get('/', (req,res)=>{
+    
+    res.sendFile(__dirname +'/resume.html');
+})
+
+app.listen(process.env.PORT || 3000,()=>{
+    console.log("web is running")
+})
